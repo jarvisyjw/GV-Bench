8f3b9f2778e600aa79bff3b1e896f64570cb0e6c
jingwen <jyubt@connect.ust.hk>
2023-12-30 14:37:52 +0800

    debug

---
+++ debug.py
@@ -0,0 +1,8 @@
+from hloc.utils.io import list_h5_names
+
+# print(len(names))
+
+if __name__ == '__main__':
+      file = '/home/jarvis/jw_ws/Verification/GV-Bench/dataset/robotcar/Night_val/features/superpoint.h5'
+      names = list_h5_names(file)
+
