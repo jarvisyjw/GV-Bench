3de1ae954973217e51d293cb34cd1c06b7b404d4
jingwen <jyubt@connect.ust.hk>
2024-03-31 14:28:25 +0800

    init commit

--- retrieval.sh
+++
@@ -1,6 +0,0 @@
-python -m hloc.pairs_from_retrieval \
-                  --descriptors dataset/robotcar/features/qAutumn_dbRain/netvlad.h5 \
-                  --query_prefix Autumn_mini_val \
-                  --db_prefix Rain_mini_val \
-                  --output dataset/robotcar/pairs/rain_test.txt \
-                  --num_matched 20
