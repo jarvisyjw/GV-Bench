bc1606d33f154e1f3cfc356ec4ebbfc6e2ce5ac5
jingwen <11710414@mail.sustech.edu.cn>
2024-05-22 20:45:33 +0800

    eval single

---
+++ main.py
@@ -0,0 +1,250 @@
+from utils import interpolate_poses, generate_sequence, plot_images, read_image, search, get_poses, plot_sequence, pre_dataset, logger
+from dataset import SeqPairsDataset, GVDataset, SeqDataset, EvaluationDataset
+from eval import seqmatch, calpr, plot_pr_curve, singlematch, max_recall, Eval_MP
+
+from pathlib import Path
+from argparse import ArgumentParser
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import random
+
+def append_df(df: pd.DataFrame, timestamps: list, seqs: list):
+      assert len(timestamps) == len(seqs), "timestamps and seqs must have the same length"
+      value = [[t] + seq for t, seq in zip(timestamps, seqs)]
+      df_append = pd.DataFrame(value, columns=['timestamp'] + [f'{i}' for i in range(len(seqs[0]))])
+      df_new = pd.concat([df, df_append], ignore_index=True)
+      df_new = df_new.sort_values(by = 'timestamp')
+      return df_new
+
+
+def plot(args):
+      # load matching results
+      exp = np.load(args.eval_output_path, allow_pickle=True).item()
+      precision = exp['precision']
+      logger.debug(f'Precision: {precision}')
+      recall = exp['recall']
+      logger.debug(f'Recall: {recall}')
+      average_precision = exp['average_precision']
+      logger.info(f'Average Precision: {average_precision}')
+      plot_pr_curve(recall, precision, average_precision, args.method, args.exp_name)
+      if args.plot_save is not None:
+            plt.savefig(args.plot_save, dpi=300, format='pdf', bbox_inches='tight', pad_inches=0.1)
+      plt.show()
+
+
+def eval_single(args):
+      '''
+      Compute the PR curve for matching with single images.
+      
+      Args:
+            args.pairs_file_path: str
+            args.matches_path: str
+            args.features: Path
+            args.output_path: Path      
+      '''
+      # dataset
+      logger.setLevel('INFO')
+      logger.info('Start Evaluation in Single Image mode...')
+      # All four sequences
+      dataset = EvaluationDataset(pairs_file= args.pairs_file_path )
+      # use multiple-process for acceleration
+      Eval_MP(10, dataset, Path(args.matches_path), 
+              Path(args.features), 
+              export_dir= Path(args.output_path, f'{Path(args.matches_path).stem}.npy'))
+
+
+
+def eval(args):
+      # dataset
+      logger.info('Loading Dataset...')
+      # dataset = GVDataset(Path(args.qImage_path), Path(args.rImage_path), args.pairs_file_path)
+      dataset = SeqPairsDataset(Path(args.qImage_path), args.rImage_path, args.qSeq_file_path, args.rSeq_file_path, args.pairs_file_path, seqL = 5)
+      matches_path = Path(args.matches_path)
+      features = Path(args.features)
+      features_ref = Path(args.features_ref)
+      ransac_output = Path(args.ransac_output)
+      # seq_match
+      # logger.info('Single Matching')
+      # singlematch(dataset, matches_path, features, features_ref, ransac_output)
+      # logger.info('Sequence Matching...')
+      seqmatch(dataset, matches_path, features, features_ref, ransac_output)
+      logger.info('Sequence Matching Done...')
+      logger.info('Calculating Precision and Recall...')
+      precision, recall, average_precision = calpr(ransac_output, args.output_path)
+      logger.info('Precision and Recall Calculation Done...')
+      _, r_recall = max_recall(precision, recall)
+      logger.info(f'\n' +
+            f'Evaluation results: \n' +
+            'Average Precision: {:.5f} \n'.format(average_precision) + 
+            'Maximum Recall @ 100% Precision: {:.5f} \n'.format(r_recall))
+      return None
+      
+
+def parser():
+      parser = ArgumentParser()
+      parser.add_argument('--poses_file', type=str)
+      parser.add_argument('--image_path', type=Path)
+      parser.add_argument('--sequence_length', type=int)
+      parser.add_argument('--gps_file', type=str)
+      parser.add_argument('--output_file', type=str)
+      parser.add_argument('--image_db', type=Path)
+      parser.add_argument('--seq_file', type=Path)
+      parser.add_argument('--dump_dir', type=Path)
+      parser.add_argument('--qImage_path', type=Path)
+      parser.add_argument('--rImage_path', type=Path)
+      parser.add_argument('--qSeq_file_path', type=str)
+      parser.add_argument('--rSeq_file_path', type=str)
+      parser.add_argument('--pairs_file_path', type=str)
+      parser.add_argument('--matches_path', type=str)
+      parser.add_argument('--features', type=Path)
+      parser.add_argument('--features_ref', type=Path)
+      parser.add_argument('--ransac_output', type=Path)
+      parser.add_argument('--output_path', type=Path)
+      parser.add_argument('--eval_output_path', type=Path)
+      parser.add_argument('--plot_save', type=str)
+      parser.add_argument('--exp_name', type=str)
+      parser.add_argument('--method', type=str)
+      parser.add_argument('--image_list', type=str)
+      parser.add_argument('--eval', action='store_true')
+      parser.add_argument('--eval_signle', action='store_true')
+      parser.add_argument('--plot', action='store_true')
+      parser.add_argument('--gen_sequence', action='store_true')
+      parser.add_argument('--interpolate_poses', action='store_true')
+      parser.add_argument('--plot_sequence', action='store_true')
+      args = parser.parse_args()
+      return args
+
+def main():
+      args = parser()
+      # Evaluation mode
+      if args.eval_single:
+            logger.info('Evaluation Process Begins...')
+            eval_single(args)
+      
+      # Plot mode
+      if args.plot:
+            logger.info('Plotting Images...')
+            plot(args)
+      
+      # Evaluation in Single Image mode
+      if args.eval:
+            eval(args)
+      
+      # Generate sequence mode
+      if args.gen_sequence:
+            logger.setLevel('INFO')
+            logger.info('Generating Sequence...')
+            dataset = GVDataset(Path(args.qImage_path), Path(args.rImage_path), args.pairs_file_path)
+            # for idx, data in enumerate(dataset):
+            #       qImages, rImages, label = data
+            #       print(qImages)
+            qImages_t = [int(data[0]) for idx, data in enumerate(dataset)]
+            _, q_error = generate_sequence(args.poses_file, qImages_t, int(args.sequence_length), args.output_file)
+            logger.info(f'Ambiguous Timestamps: {q_error}')
+            
+      
+      # Interpolate poses
+      if args.interpolate_poses:
+            interpolate_poses(args.image_path, args.gps_file, args.output_file)
+      
+      
+      # Plot sequence
+      if args.plot_sequence:
+            dataset = SeqDataset(Path(args.qImage_path), Path(args.rImage_path), Path(args.qSeq_file_path), 
+                                 Path(args.rSeq_file_path), args.pairs_file_path)
+            if args.image_list is not None:
+                  image_list = [int(image) for image in args.image_list.split(',')]
+            else:
+                  image_list = [random.randint(0, len(dataset)-1) for i in range(5)]
+                  for image in image_list:
+                        qImages, rImages, label = dataset[image]
+                        plot_sequence([qImages, rImages], label=label)
+            
+      
+      '''
+      Prepare the dataset for the sequence
+      '''
+      # pre_dataset(args.image_path, args.seq_file, args.dump_dir)
+      
+      
+      '''
+      Generate sequences
+      '''
+      # image_t = sorted([int(image.name.strip('.jpg')) for image in args.image_path.iterdir()])
+      # _, error = generate_sequence(args.poses_file, image_t, args.sequence_length, args.output_file)
+      # print(f'Ambiguous Timestamps: {error}')
+      
+      '''
+      Mannually append sequences to the dataframe
+      '''
+      # df = pd.read_csv(args.seq_file)
+      # forward_ts = [1418757503567203, 1418757503879662, 1418757504504577, 1418757504879527, 1418757508566532]
+      # forward_seqs = [[1418757498567877, 1418757499630232, 1418757500880066, 1418757502192390, 1418757503567203],
+      #                   [1418757498817843, 1418757499880200, 1418757501130031, 1418757502504848, 1418757503879662],
+      #                   [1418757499255285, 1418757500380133, 1418757501692455, 1418757503067272, 1418757504504577],
+      #                   [1418757499442760, 1418757500630099, 1418757501942423, 1418757503317238, 1418757504879527],
+      #                   [1418757500630099, 1418757501942423, 1418757503317238, 1418757504942019, 1418757508566532]]
+      # backward_ts = [1418134766291893, 1418134766541851, 1418134766791828, 1418134767041806, 1418134767291782, 1418134767604234, 1418134767916680, 1418134768291636, 1418134768666609, 1418134769104043, 1418134769603963, 1418134770166372, 1418134770791314, 1418134771603697, 1418134772603561, 1418134774103318, 1418134776665509]
+      # backward_seqs = [[1418134762604895, 1418134763479748, 1418134764354604, 1418134765291966, 1418134766291893],
+      #                   [1418134762729876, 1418134763604728, 1418134764479585, 1418134765479453, 1418134766541851],
+      #                   [1418134762979833, 1418134763854688, 1418134764729542, 1418134765729431, 1418134766791828],
+      #                   [1418134763104811, 1418134763979668, 1418134764917011, 1418134765916920, 1418134767041806],
+      #                   [1418134763292278, 1418134764167137, 1418134765104481, 1418134766166904, 1418134767291782],
+      #                   [1418134763542238, 1418134764417094, 1418134765354458, 1418134766416871, 1418134767604234],
+      #                   [1418134763604728, 1418134764479585, 1418134765479453, 1418134766604340, 1418134767916680],
+      #                   [1418134763854688, 1418134764729542, 1418134765729431, 1418134766854318, 1418134768291636],
+      #                   [1418134763979668, 1418134764917011, 1418134765916920, 1418134767104306, 1418134768666609],
+      #                   [1418134764167137, 1418134765104481, 1418134766166904, 1418134767354275, 1418134769104043],
+      #                   [1418134764354604, 1418134765291966, 1418134766354382, 1418134767604234, 1418134769603963],
+      #                   [1418134764479585, 1418134765479453, 1418134766604340, 1418134767916680, 1418134770166372],
+      #                   [1418134764729542, 1418134765729431, 1418134766854318, 1418134768291636, 1418134770791314],
+      #                   [1418134764854520, 1418134765854428, 1418134767041806, 1418134768604118, 1418134771603697],
+      #                   [1418134765041991, 1418134766104406, 1418134767291782, 1418134769104043, 1418134772603561],
+      #                   [1418134765229467, 1418134766291893, 1418134767541743, 1418134769541474, 1418134774103318],
+      #                   [1418134765416955, 1418134766541851, 1418134767854191, 1418134770041392, 1418134776665509]]
+
+      # timestamps = forward_ts
+      # seqs = forward_seqs
+      
+      # df_new = append_df(df, timestamps, seqs)
+      # df_new.to_csv(args.output_file, index=False)
+
+
+      '''
+      Mannually search for sequences
+      '''
+      # interpolate_poses(args.image_path, args.gps_file, args.output_file)
+      
+      # timestamps , poses = get_poses(args.poses_file)
+      
+      # forward_search = forward_search = [1416316985484274, 1416316986234142, 1416317013667830, 1416317014042806, 1416317014355275, 1416317014667735, 1416317014980179, 1416317015230153, 1416317015480119]
+      # backward_search = backward_search = [1416319227487911, 1416319227925377, 1416319228362851, 1416319228862826, 1416319229487770, 1416319230237662, 1416319230925047, 1416319231674920, 1416319232549828, 1416319233549720, 1416319234924496, 1416319237299109]
+      
+      '''
+      Backward Search
+      '''
+      # for t in backward_search:
+      #       center = bisect.bisect(timestamps, t)
+      #       seq = search(poses, timestamps, 4, center, -1)
+      #       seq.append(timestamps[center-1])
+      #       # seq.insert(0, timestamps[center-1])
+      #       print(seq)
+      #       plot_sequence([[read_image(image_path / f'{t}.jpg') for t in seq]])
+      #       plt.show()
+      
+      '''
+      Forward Search
+      '''
+      # for t in forward_search:
+      #       center = bisect.bisect(timestamps, t)
+      #       seq = search(poses, timestamps, 4, center, 1)
+      #       # seq.append(timestamps[center-1])
+      #       seq.insert(0, timestamps[center-1])
+      #       print(seq)
+      #       plot_sequence([[read_image(image_path / f'{t}.jpg') for t in seq]])
+      #       plt.show()
+      
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,353 @@
+from pathlib import Path
+import logging
+import pandas as pd
+from tqdm import tqdm
+import numpy as np
+import bisect
+import csv
+import numpy as np
+import numpy.matlib as ml
+import warnings
+import matplotlib.pyplot as plt
+import cv2
+import shutil
+import h5py
+from typing import Tuple
+
+
+from transform import se3_to_components
+from interpolate_poses import interpolate_ins_poses
+
+
+
+### logger
+formatter = logging.Formatter(
+    fmt='[%(asctime)s %(name)s %(levelname)s] %(message)s',
+    datefmt='%Y/%m/%d %H:%M:%S')
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+
+logger = logging.getLogger("GV-Bench")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+### Some of the functions are copied from hloc
+def read_image(path, grayscale=False):
+    if grayscale:
+        mode = cv2.IMREAD_GRAYSCALE
+    else:
+        mode = cv2.IMREAD_COLOR
+    image = cv2.imread(str(path), mode)
+    if image is None:
+        raise ValueError(f'Cannot read image {path}.')
+    if not grayscale and len(image.shape) == 3:
+        image = image[:, :, ::-1]  # BGR to RGB
+    return image
+
+
+def names_to_pair(name0, name1, separator='/'):
+    return separator.join((name0.replace('/', '-'), name1.replace('/', '-')))
+
+def names_to_pair_old(name0, name1):
+    return names_to_pair(name0, name1, separator='_')
+
+
+def find_pair(hfile: h5py.File, name0: str, name1: str):
+    pair = names_to_pair(name0, name1)
+    if pair in hfile:
+        return pair, False
+    pair = names_to_pair(name1, name0)
+    if pair in hfile:
+        return pair, True
+    # older, less efficient format
+    pair = names_to_pair_old(name0, name1)
+    if pair in hfile:
+        return pair, False
+    pair = names_to_pair_old(name1, name0)
+    if pair in hfile:
+        return pair, True
+    raise ValueError(
+        f'Could not find pair {(name0, name1)}... '
+        'Maybe you matched with a different list of pairs? ')
+
+
+def get_keypoints(path: Path, name: str,
+                  return_uncertainty: bool = False) -> np.ndarray:
+    with h5py.File(str(path), 'r', libver='latest') as hfile:
+        dset = hfile[name]['keypoints']
+        p = dset.__array__()
+        uncertainty = dset.attrs.get('uncertainty')
+        # print('uncertaintylistlens', len(uncertainty))
+    if return_uncertainty:
+        return p, uncertainty
+    return p
+
+
+def get_matches(path: Path, name0: str, name1: str, out=None) -> Tuple[np.ndarray]:
+    with h5py.File(str(path), 'r', libver='latest') as hfile:
+        pair, reverse = find_pair(hfile, name0, name1)
+        matches = hfile[pair]['matches0'].__array__()
+        scores = hfile[pair]['matching_scores0'].__array__()
+    idx = np.where(matches != -1)[0]
+    matches = np.stack([idx, matches[idx]], -1)
+    if reverse:
+        matches = np.flip(matches, -1)
+    scores = scores[idx]
+    if out is not None:
+        matches_score = np.column_stack((matches, scores))
+        with open(out, 'w') as f:
+            f.write('\n'.join(' '.join(map(str, match)) for match in matches_score))
+        f.close()
+    return matches, scores
+
+
+def plot_images(imgs, titles=None, cmaps='gray', dpi=100, pad=.5,
+                adaptive=True, figsize=4.5):
+    """Plot a set of images horizontally.
+    Args:
+        imgs: a list of NumPy or PyTorch images, RGB (H, W, 3) or mono (H, W).
+        titles: a list of strings, as titles for each image.
+        cmaps: colormaps for monochrome images.
+        adaptive: whether the figure size should fit the image aspect ratios.
+    """
+    n = len(imgs)
+    if not isinstance(cmaps, (list, tuple)):
+        cmaps = [cmaps] * n
+
+    if adaptive:
+        ratios = [i.shape[1] / i.shape[0] for i in imgs]  # W / H
+    else:
+        ratios = [4/3] * n
+    figsize = [sum(ratios)*figsize, figsize]
+    fig, axs = plt.subplots(
+        1, n, figsize=figsize, dpi=dpi, gridspec_kw={'width_ratios': ratios})
+    if n == 1:
+        axs = [axs]
+    for i, (img, ax) in enumerate(zip(imgs, axs)):
+        ax.imshow(img, cmap=plt.get_cmap(cmaps[i]))
+        ax.set_axis_off()
+        if titles:
+            ax.set_title(titles[i])
+    fig.tight_layout(pad=pad)
+    
+
+def plot_sequence(images, figsize=(15, 10), dpi=100, pad=.5, show = True, label = None):
+    """Plot a set of image sequences where horizontal images are from the same sequence.
+    Args:
+        images: a list of list of NumPy or PyTorch images, RGB (H, W, 3) or mono (H, W).
+        
+    """
+    n = len(images) # num of sequences
+    l = len(images[0]) # num of images in each sequence
+    
+    fig, axs = plt.subplots(n, l, figsize=figsize, dpi=dpi)
+    
+    if n == 1:
+        axs = [axs]
+    for i, (image, ax) in enumerate(zip(images, axs)):
+        for j, img in enumerate(image):
+            ax[j].imshow(img)
+            # ax[j].set_title(f'Image {j}')
+        # ax.imshow(image)
+            ax[j].set_axis_off()
+    fig.tight_layout(pad=pad)
+    if label is not None:
+        fig.suptitle(f'This is a {label} pair.', fontsize=16)
+    if show:
+        plt.show()
+
+
+def parse_pairs(file: Path, allow_label = False):
+
+    logger.info(f'Loading ground truth from {file}')
+    logger.info(f'Allow label: {allow_label}')
+    if isinstance(file, Path):
+          file = str(file)
+    
+    f = open(file, 'r')
+    for line in f.readlines():
+        line = line.strip('\n')
+        if line.startswith('#'):
+            continue
+        else:
+            line = line.split(' ')
+            if allow_label:
+                  query, reference, label = line
+                  yield query, reference, label
+            else:
+                  query, reference = line
+                  yield query, reference
+    f.close()
+    
+
+def interpolate_poses(image_path: Path, gps_file: str, output: str):
+    
+    image_t = sorted([int(image.name.strip('.jpg')) for image in image_path.iterdir()])
+    
+    ins_pd = pd.read_csv(gps_file)
+    ts = ins_pd['timestamp'].tolist()
+    t_ref = ts[0]
+
+    dataframe = []
+    _, poses = interpolate_ins_poses(gps_file, image_t, t_ref)
+
+    for t, pose in tqdm(zip(image_t, poses), total=len(image_t)):
+        xyzrpy = se3_to_components(pose)
+        # xyzrpy to list
+        xyzrpy = xyzrpy.tolist()
+        # print(xyzrpy, isinstance(xyzrpy, list))
+        # np.set_printoptions(precision=12, suppress=True)
+        out = [t, *xyzrpy]
+        dataframe.append(out)
+
+    df = pd.DataFrame(dataframe, columns=['timestamp', 'x', 'y', 'z', 'roll', 'pitch', 'yaw'])
+    df.to_csv(output, index=False)
+    return df
+
+
+def get_poses(poses_file: str):
+    poses = [np.zeros(6)]
+    timestamps = [0]
+    
+    with open(poses_file) as poses_file:
+        poses_reader = csv.reader(poses_file)
+        headers = next(poses_file)
+
+        for row in poses_reader:
+            timestamp = int(row[0])
+            utm = row[1:4]
+            rpy = row[-3:]
+            xyzrpy = [float(v) for v in utm] + [float(v) for v in rpy]
+            timestamps.append(timestamp)
+            poses.append(xyzrpy)
+    
+    return timestamps[1:], poses[1:]
+
+
+def dist_matrix(pose_file: str):
+    pass
+
+
+
+def generate_sequence(poses_file: str, origin: list, length: int, output_file: str):
+    # TODO: out of boundary check
+    # day0 not working
+    # manually process now
+    
+    timestamps, poses = get_poses(poses_file)
+    centers = [bisect.bisect(timestamps, t) for t in origin]
+    
+    logger.debug(f'Max idx: {max(centers)}')
+
+    if max(centers) > len(timestamps):
+        centers = [min(i, len(timestamps) - 1) for i in centers]
+    
+    if length%2 == 0:
+        raise ValueError('Length must be odd')
+    
+    seq_out = []
+    
+    for i in tqdm(centers):
+        seq = find_sequence(timestamps, poses, i, length)
+        seq_out.append(seq)
+    
+    frames = []
+    errors = []
+    for t, seq in zip(origin, seq_out):
+        if seq is not None:
+            frames.append([t] + seq)
+        else:
+            errors.append(t)
+    
+    columns = ['timestamp'] + [f'{i}' for i in range(length)]
+    pd.DataFrame(frames, columns=columns).to_csv(output_file, index=False)
+    
+    return seq_out, errors
+
+
+def find_sequence(timestamps: list, poses: list, idx: int, length: int):
+    '''
+    Find sequence of timestamps given the current timestamp
+    '''
+    
+    seq = []
+    forward_seq = []
+    backward_seq = []
+
+    # forward step
+    steps = length//2
+    
+    logger.debug(f'Length: {length}, Steps: {steps}')
+    logger.debug(f'Current index: {idx}')
+    logger.debug(f'Length of all sequence: {len(timestamps)}')
+    logger.debug(f'Current timestamp: {timestamps[idx]}')
+    
+    if check_dist(poses[idx], poses[len(timestamps)-1], distance_threshold=steps * 5) and check_dist(poses[idx], poses[0], distance_threshold=steps * 5):
+        # search both forward and backward
+        forward_seq = search(poses, timestamps, steps, idx, 1)
+        backward_seq = search(poses, timestamps, steps, idx, -1)
+        seq = backward_seq + [timestamps[idx-1]] + forward_seq
+        # seq = backward_seq[::-1] + [timestamps[idx-1]] + forward_seq
+    
+    elif check_dist(poses[idx], poses[len(timestamps)-1], distance_threshold=(length-1) * 5):
+        # search forward
+        seq = search(poses, timestamps, length-1, idx, 1)
+        seq = seq.insert(0, timestamps[idx-1])
+        
+    else:
+        # search backwoard
+        seq = search(poses, timestamps, length-1, idx, -1)
+        seq = seq.append(timestamps[idx-1])
+    
+    return seq
+
+
+def search(poses: list, timestamps: list, steps: int, center: int, direction = 1):
+    # direction 1 for forward search
+    # direction -1 for backward search
+    seq = []
+    pt = center + direction
+    
+    while steps > 0:
+        if check_dist(poses[pt], poses[center]):
+            if direction == -1:
+                seq.insert(0, timestamps[pt])
+            else:
+                seq.append(timestamps[pt])
+            center = pt
+            pt = pt + direction
+            steps -= 1
+        else:
+            pt = pt + direction
+            
+    return seq
+
+
+def check_dist(xyzrpy, xyzrpy_ref, distance_threshold=5, large = True):
+    '''
+    Check if the distance between the current timestamp and the next timestamp is greater than 0.5s
+    '''
+    
+    xyzrpy = np.array([float(p) for p in xyzrpy])
+    xyzrpy_ref = np.array([float(p) for p in xyzrpy_ref])
+    dist = np.linalg.norm(xyzrpy[:2] - xyzrpy_ref[:2])
+    
+    if large: 
+        return False if dist < distance_threshold else True
+    else: 
+        return True if dist < distance_threshold else False
+    
+
+def pre_dataset(image_path: Path, sequence_file: str, dump_dir: Path):
+    '''
+    Prepare the dataset for the sequence
+    '''
+    df = pd.read_csv(sequence_file)
+    for i in tqdm(range(len(df.columns)-1)):
+        images = df[str(i)].tolist()
+        for image in images:
+            shutil.copyfile(Path(image_path, f'{image}.jpg'), Path(dump_dir, f'{image}.jpg'))
+    
+    logger.info(f'Dataset prepared at {dump_dir}')
